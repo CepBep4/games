5b8b5b6ba5939cf1f45d10b40e70a2e219724993
Артём <sever@MacBook-Air-Artem-2.local>
2024-12-27 00:44:34 +0300

    0.0.2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='chessgame/dist', static_folder='chessgame/dist/assets')
 
+
 @app.route('/')
 def home():
     return render_template('choice.html')
